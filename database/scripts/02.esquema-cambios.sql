25e39b0006944f39bd9c223a3ad0f54c200ff842
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-11-23 13:51:52 -0600

    Se agregó una tabla para la configuración de los módulos

---
+++ database/scripts/02.esquema-cambios.sql
@@ -0,0 +1,12 @@
+/* CREAR TABLA PARA LA CONFIGURACIÓN DE LOS SISTEMAS O MÓDULOS */
+DROP TABLE IF EXISTS conf_sistema;
+CREATE TABLE conf_sistema
+(
+    conf_sistema_id         INT             NOT NULL AUTO_INCREMENT,
+    nombre                  VARCHAR(100)   NOT NULL,
+    abreviatura             VARCHAR(10)    NOT NULL,
+    fecha_inicial           DATE            NULL,
+    fecha_final             DATE            NULL,
+    estado                  BIT             NOT NULL,
+    CONSTRAINT pk_conf_sistema PRIMARY KEY (conf_sistema_id ASC)
+);
