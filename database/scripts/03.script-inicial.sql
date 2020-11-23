630572315536fd39f694de845015dcb9b2d6c130
MaggieSierra <43976301+MaggieSierra@users.noreply.github.com>
2020-11-23 13:59:19 -0600

    Insert de la configuración del módulo de reinscripción

--- database/scripts/03.script-inicial.sql
+++ database/scripts/03.script-inicial.sql
@@ -4,14 +4,21 @@ USE wuac69fxggq49kmt;
  * *********** INSERTS DE TABLA DE NIVELES *********** *
  * *************************************************** */
 INSERT INTO nivel (nombre)
-VALUES  ('Licenciatura'), 
-        ('Maestría'), 
-        ('Doctorado'), 
+VALUES  ('Licenciatura'),
+        ('Maestría'),
+        ('Doctorado'),
         ('Externo');
 
 /* *************************************************** *
  * ************ INSERTS DE TABLA DE ROLES ************ *
  * *************************************************** */
-INSERT INTO rol (nombre, abreviatura) 
+INSERT INTO rol (nombre, abreviatura)
 VALUES  ('Administrador', 'ADMIN'),
         ('Estudiante', 'ESTU');
+
+
+/* *************************************************** *
+ * ** INSERTS DE TABLA DE CONFIGURACÍON DEL SISTEMA ** *
+ * *************************************************** */
+INSERT INTO conf_sistema (nombre, abreviatura, estado)
+VALUES ('MÓDULO DE REINSCRIPCION', 'MOD_REINS', 1);
